28b731433ce66b4c7a78f92e7f8b7227afd1e0c3
Divya <divyadhamija0101@gmail.com>
2024-07-08 18:51:20 +0530

    Added Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+function Footer()
+{
+    return (
+        <div style={styles}>
+        <p>All rights Reserved </p>
+        </div>
+    );
+}
+
+const styles={
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    backgroundColor: '#f1f1f1',
+    textAlign: 'center',
+};
+
+export default Footer
